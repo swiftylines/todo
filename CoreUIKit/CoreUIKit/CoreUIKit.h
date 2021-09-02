5b0e4dcdc4408666ca8724ce5cfbaaf8ca25ee90
0vertex <>
2021-09-02 22:35:59 +0530

    Added core uikit framework

---
+++ CoreUIKit/CoreUIKit/CoreUIKit.h
@@ -0,0 +1,18 @@
+//
+//  CoreUIKit.h
+//  CoreUIKit
+//
+//  Created by Manish on 02/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreUIKit.
+FOUNDATION_EXPORT double CoreUIKitVersionNumber;
+
+//! Project version string for CoreUIKit.
+FOUNDATION_EXPORT const unsigned char CoreUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreUIKit/PublicHeader.h>
+
+
