764eaccf208f685f6835e80080434cdc2571f016
0vertex <>
2021-09-19 12:35:03 +0530

    Added tests for BaseTableView and docs.

--- CoreUIKit/CoreUIKit/BaseTableView/BaseTableView.swift
+++ CoreUIKit/CoreUIKit/BaseTableView/BaseTableView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/// A view replacing direct use of `UITableView` with added features.
 public class BaseTableView: UITableView {
     
     public init(with pullToRefresh: Bool = false) {
@@ -25,15 +26,25 @@ public class BaseTableView: UITableView {
 // MARK: - Register and Dequeue cells
 public extension BaseTableView {
     
+    /// Registeres provided cell type with current `BaseTableView` instance
+    /// - Parameter cellType: type of `UITableViewCell`, that needs to be registered
     func register<CellType: UITableViewCell>(_ cellType: CellType.Type) {
         self.register(cellType,
                       forCellReuseIdentifier: String(describing: cellType))
     }
     
+    /// Dequeues reusable registered cell type
+    /// - Parameter cellType: type of `UITableViewCell`, that needs to be dequeued
+    /// - Returns: Cell instance, if it was regstered or `nil`
     func dequeue <CellType: UITableViewCell>(_ cellType: CellType.Type) -> CellType? {
         return self.dequeueReusableCell(withIdentifier: String(describing: cellType)) as? CellType
     }
     
+    /// Dequeues reusable registered cell type
+    /// - Parameters:
+    ///   - cellType: type of `UITableViewCell`, that needs to be dequeued
+    ///   - indexPath: dequeue for position
+    /// - Returns: Cell instance, if it was regstered or `nil`
     func dequeue <CellType: UITableViewCell>(_ cellType: CellType.Type,
                                              for indexPath: IndexPath) -> CellType? {
         return self.dequeueReusableCell(withIdentifier: String(describing: cellType),
