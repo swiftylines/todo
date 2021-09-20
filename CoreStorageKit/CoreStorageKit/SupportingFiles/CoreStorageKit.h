5f5fa2ef34d9c71901949e944ecc10312905639b
0vertex <>
2021-09-20 21:06:39 +0530

    Added core storage kit

---
+++ CoreStorageKit/CoreStorageKit/SupportingFiles/CoreStorageKit.h
@@ -0,0 +1,18 @@
+//
+//  CoreStorageKit.h
+//  CoreStorageKit
+//
+//  Created by Manish on 20/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreStorageKit.
+FOUNDATION_EXPORT double CoreStorageKitVersionNumber;
+
+//! Project version string for CoreStorageKit.
+FOUNDATION_EXPORT const unsigned char CoreStorageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreStorageKit/PublicHeader.h>
+
+
