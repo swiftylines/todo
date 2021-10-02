2b7ea04045e1a1caa8dd058d03b41e3390778ef4
0vertex <>
2021-10-02 22:03:49 +0530

    Added intial setups for local storage features of the todo items.

---
+++ ToDoApp/ToDoApp/Modules/Helpers/ToDoListStorageHelper.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListStorageHelper.swift
+//  ToDoApp
+//
+//  Created by Manish on 02/10/21.
+//
+
+import Foundation
+
+
+/// Provides todo storage features, such as creating and deleting a todo item
+///
+/// - Important
+///     - It's not recommended to use  `ToDoListStorageHelper` directly, because it will directly perform the operation with the provided data.
+///
+class ToDoListStorageHelper {
+    
+    
+    
+}
